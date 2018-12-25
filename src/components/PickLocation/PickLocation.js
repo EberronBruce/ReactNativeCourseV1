d7dc8bbd755874a6fc845db560655359f4c2e1bf
EberronBruce <bruce@redravencomputing>
2018-12-24 20:38:56 -0700

    Render maps on Share Place screen

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,16 +1,31 @@
 //@flow
 import React, {Component} from 'react';
-import { View, Image, Button, StyleSheet, Text } from 'react-native';
+import { View, Image, Button, StyleSheet, Text, Dimensions } from 'react-native';
+import MapView from "react-native-maps";
 
 type Props = {};
+type State = {focusedLocation: Object}
 
-class PickLocation extends Component<Props> {
+class PickLocation extends Component<Props, State> {
+  state ={
+    focusedLocation: {
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        Dimensions.get("window").width /
+        Dimensions.get("window").height *
+        0.0122
+    }
+  }
+
   render() {
     return (
     <View style={styles.container}>
-      <View style={styles.placeholder}>
-        <Text>Map</Text>
-      </View>
+      <MapView
+        initialRegion={this.state.focusedLocation}
+        style={styles.map}
+      />
       <View style={styles.button}>
         <Button title="Locate Me" onPress={() => alert('Pick Location!')}/>
       </View>
@@ -24,12 +39,9 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: 'center'
   },
-  placeholder: {
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "#eee",
-    width: "80%",
-    height: 150
+  map: {
+    width: "100%",
+    height: 250
   },
   button: {
     margin: 8
