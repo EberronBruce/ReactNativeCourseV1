38da3ba2d4324d2314bf00f678032186de82ed9d
EberronBruce <bruce@redravencomputing>
2018-12-28 23:27:05 -0700

    Setup Up signing in users

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,8 +2,31 @@
 import {TRY_AUTH} from './actionTypes';
 
 export const tryAuth = (authData: Object) => {
-  return {
-    type: TRY_AUTH,
-    authData: authData
+  return (dispatch : Function) => {
+    dispatch(authSignup(authData));
+  };
+};
+
+export const authSignup = (authData : Object) => {
+  return (dispatch : Function) => {
+    fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAGCh5InBaq0M3ICx_2So5UJIeo2J0vgPk",{
+      method: "POST",
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      alert("Authentication failed, please try again");
+    })
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
+    })
   };
 };
