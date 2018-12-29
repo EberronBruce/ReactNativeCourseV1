79bb6d61110e314dc33673121510d54d8617395d
EberronBruce <bruce@redravencomputing>
2018-12-29 06:25:11 -0700

    Reseting Share Place

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -9,18 +9,24 @@ type State = {focusedLocation: Object, locationChosen: bool}
 
 class PickLocation extends Component<Props, State> {
 
-  state ={
-    focusedLocation: {
-      latitude: 37.7900352,
-      longitude: -122.4013726,
-      latitudeDelta: 0.0122,
-      longitudeDelta:
-        Dimensions.get("window").width /
-        Dimensions.get("window").height *
-        0.0122
-    },
-    locationChosen: false
-  };
+  componentWillMount() {
+    this.reset();
+  }
+
+  reset = () => {
+    this.setState({
+      focusedLocation: {
+        latitude: 37.7900352,
+        longitude: -122.4013726,
+        latitudeDelta: 0.0122,
+        longitudeDelta:
+          Dimensions.get("window").width /
+          Dimensions.get("window").height *
+          0.0122
+      },
+      locationChosen: false
+    });
+  }
 
   pickLocationHandler = (event: Object) => {
     const coords = event.nativeEvent.coordinate;
@@ -74,6 +80,7 @@ class PickLocation extends Component<Props, State> {
     <View style={styles.container}>
       <MapView
         initialRegion={this.state.focusedLocation}
+        region={this.state.locationChosen ? this.state.focusedLocation : null}
         style={styles.map}
         onPress={this.pickLocationHandler}
         ref={ref => this.map = ref}
